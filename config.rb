48f93905a7137844370f3f4764af6a19ec6bb9f0
Ryan Collier <rdcollier@gmail.com>
2016-02-15 12:08:28 -0800

    Add S3 Sync to config

--- config.rb
+++ config.rb
@@ -58,6 +58,25 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'ryancollier.co' # The name of the S3 bucket you are targetting. This is globally unique.
+  s3_sync.region                     = 'Oregon'     # The AWS region for your bucket.
+# s3_sync.aws_access_key_id          = 'AWS KEY ID'
+# s3_sync.aws_secret_access_key      = 'AWS SECRET KEY'
+  s3_sync.delete                     = false # We delete stray files by default.
+  s3_sync.after_build                = false # We do not chain after the build step by default.
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  s3_sync.reduced_redundancy_storage = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+  s3_sync.prefix                     = ''
+  s3_sync.version_bucket             = false
+  s3_sync.index_document             = 'index.html'
+  s3_sync.error_document             = '404.html'
+end
+
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
